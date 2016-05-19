8b4642955f2ee111c55ed52664e54d4ccf44df51
luis <luis.9993@gmail.com>
2016-05-19 13:43:01 -0400

    Grafico de los impulsos convolucionada con sinc

--- laboratorio3.py
+++ laboratorio3.py
@@ -6,7 +6,26 @@ import matplotlib
 from scipy.fftpack import fft, fftfreq, ifft
 from scipy.signal import *
 
-f, plots = plt.subplots(2)
+def random_array(n, zeros):
+	a = np.random.randint(2, size=n)
+	b = []
+	j=0
+	for i in range(len(a)):
+		while j<zeros:
+			b.append(0)
+			j+=1
+		j=0
+		if(a[i]==0):
+			b.append(-1)
+		else:
+			b.append(1)
+	while j<zeros:
+			b.append(0)
+			j+=1
+	return b
+
+
+f, plots = plt.subplots(3)
 
 # Grafico debe estar normalizado 
 # Eyediagram
@@ -16,7 +35,8 @@ f, plots = plt.subplots(2)
 
 # Tiempo-> 5 periodos ??
 # Sinc
-T = 50
+
+T = 80
 Fs = 1/T
 print(Fs)
 x = np.linspace(-T, T, (T/Fs))
@@ -30,10 +50,19 @@ transformada = abs(fft(signal))
 transformada = transformada*Fs*T
 freqs = np.fft.fftfreq(len(x), Fs)
 #freqs = np.fft.fftshift(freqs)
-plots[1].plot(freqs, transformada/(T*0.5), 'r')
+"""plots[1].plot(freqs, transformada/(T*0.5), 'r')
+f.show()
+a = []
+print(x)"""
+signal2 = random_array(10, T)
+conv = np.convolve(signal, signal2)
+plots[1].plot(signal2)
+plots[0].grid(True)
+plots[2].plot(conv)
+plots[2].grid(True)
 f.show()
 
-print(x)
+
 
 input("Presione enter para salir:\n")
 
