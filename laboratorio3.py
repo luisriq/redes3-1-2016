6d27383820d5939bd37809f99dd985b1ca90d43c
Cristian Espinoza Leon <cristian.espinozal@usach.cl>
2016-05-23 11:58:15 -0400

    casi listo parte 1
    ordenar y poner + cmentarios

--- laboratorio3.py
+++ laboratorio3.py
@@ -39,7 +39,7 @@ def fun_prc(T, a):
 	print(signal)
 	return Fs, x, signal
 
-f, plots = plt.subplots(3)
+f, plots = plt.subplots(2)
 
 # Grafico debe estar normalizado 
 # Eyediagram
@@ -55,25 +55,40 @@ T=80
 Fs, x, signal = fun_sinc(T)
 Fs2, x2, signal_rc_25 = fun_prc(T, 0.25)
 Fs2, x2, signal_rc_5 = fun_prc(T, 0.5)
-Fs2, x2, signal_rc_1 = fun_prc(T, 1)
-
+Fs2, x2, signal_rc_1 = fun_prc(T, 0.99)
+#graficos en funcion del tiempo
 plots[0].plot(x, signal, 'b')
 plots[0].plot(x2, signal_rc_25, 'g')
 plots[0].plot(x2, signal_rc_5, 'r')
 plots[0].plot(x2, signal_rc_1, 'c')
 plots[0].grid(True)
+
+# Fourier sinc      		el /T*0.5 es para normalizar la transformada
+transformada = abs(fft(signal))
+transformada = transformada/(T*0.5)
+transformada_rc_25 = abs(fft(signal_rc_25))
+transformada_rc_25 = transformada_rc_25/(T*0.5)
+transformada_rc_5 = abs(fft(signal_rc_5))
+transformada_rc_5 = transformada_rc_5/(T*0.5)
+transformada_rc_1 = abs(fft(signal_rc_1))
+transformada_rc_1 = transformada_rc_1/(T*0.5)
+freqs = np.fft.fftfreq(len(x), Fs)
+#graficos en frecuencia
+plots[1].plot(freqs, transformada, 'b')
+plots[1].plot(freqs, transformada_rc_25, 'g')
+plots[1].plot(freqs, transformada_rc_5, 'r')
+plots[1].plot(freqs, transformada_rc_1, 'c')
+plots[1].set_xlim([-3,3])	#xq asi da bonito
 #end parte 1
 
 
-# Fourier sinc
-transformada = abs(fft(signal))
-transformada = transformada*Fs*T
-freqs = np.fft.fftfreq(len(x), Fs)
+
+#parte 2
 signal2 = random_array(10, T)
 conv = np.convolve(signal, signal2)
-plots[1].plot(signal2)
-plots[2].plot(conv)
-plots[2].grid(True)
+# plots[1].plot(signal2)
+# plots[2].plot(conv)
+# plots[2].grid(True)
 
 f.show()
 
