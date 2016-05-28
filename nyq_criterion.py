7ca4b1aa6a39e1b17f4de034840b7782ebdfa96b
Fireness <firenessmax@gmail.com>
2016-05-28 13:07:32 -0300

    arreglado criterio de arreglo de función para todo T donde el alfa hace que sea 0

--- nyq_criterion.py
+++ nyq_criterion.py
@@ -4,9 +4,9 @@ import math
 from scipy.fftpack import fft, fftfreq, ifft
 
 def nyq_criterion(a,T,error=1e-12):
-	if(a==.25):
-		a=.25+1e-12
 	for n in range(-T,T):
+		if((1-((4*(a**2)*(n**2))))==0):
+			a=a+1e-12
 		prc_t =  np.sinc(n) * np.cos(a*np.pi*n)/(1-((4*(a**2)*(n**2))))
 		if(n==0 and abs(prc_t-1)<=error):
 			continue
